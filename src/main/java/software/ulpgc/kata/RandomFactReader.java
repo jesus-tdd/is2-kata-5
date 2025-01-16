6c3733957737a87d5105c154f661ea0b1c7f26a7
Jesus <jesus.tdd03@gmail.com>
2025-01-16 19:27:59 +0000

    Created class RandomFactReader

---
+++ src/main/java/software/ulpgc/kata/RandomFactReader.java
@@ -0,0 +1,22 @@
+package software.ulpgc.kata;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+
+public class RandomFactReader implements FactReader {
+    @Override
+    public String read() throws IOException {
+        return read(FactApi.GET_RANDOM_FACT);
+    }
+
+    private String read(String url) throws IOException {
+        Connection.Response response = Jsoup.connect(url)
+                .ignoreContentType(true)
+                .method(Connection.Method.GET)
+                .execute();
+        if(response.statusCode() != 200) throw new RuntimeException();
+        return response.body();
+    }
+}
